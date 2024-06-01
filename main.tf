c273feb15497c56ebe59365428ba07c2df74be62
Akash-1704 <90324028+Akash-1704@users.noreply.github.com>
2024-06-01 23:46:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,20 +2,14 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"
-}
-
 module "vpc-ap-south-1" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.14.0"
+  source = "terraform-aws-modules/vpc/aws"
 
   name = var.aws_vpc_name
   cidr = var.aws_vpc_cidr
 
-  azs            = var.aws_vpc_azs["ap-south-1"]
-  public_subnets = var.aws_vpc_public_subnets
+  azs             = var.aws_vpc_azs["ap-south-1"]
+  public_subnets  = var.aws_vpc_public_subnets
 
   tags = {
     Name = "vpc-ap-south-1"
@@ -23,14 +17,13 @@ module "vpc-ap-south-1" {
 }
 
 module "vpc-us-east-1" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.14.0"
+  source = "terraform-aws-modules/vpc/aws"
 
   name = var.aws_vpc_name
   cidr = var.aws_vpc_cidr
 
-  azs            = var.aws_vpc_azs["us-east-1"]
-  public_subnets = var.aws_vpc_public_subnets
+  azs             = var.aws_vpc_azs["us-east-1"]
+  public_subnets  = var.aws_vpc_public_subnets
   
   providers = {
     aws = aws.us_east_1
@@ -41,21 +34,25 @@ module "vpc-us-east-1" {
   }
 }
 
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
+
 module "ec2_ap_south_1" {
   source        = "./modules/ec2"
+  region        = "ap-south-1"
   instance_type = var.instance_type
   subnet_id     = element(module.vpc-ap-south-1.public_subnets, 0)
   ami_id        = var.ami_id["ap-south-1"]
-  region        = "ap-south-1"
 }
 
 module "ec2_us_east_1" {
   source        = "./modules/ec2"
-  providers     = {
+  providers = {
     aws = aws.us_east_1
   }
   instance_type = var.instance_type
   subnet_id     = element(module.vpc-us-east-1.public_subnets, 0)
   ami_id        = var.ami_id["us-east-1"]
-  region        = "us-east-1"
 }
