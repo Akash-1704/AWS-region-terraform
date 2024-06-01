72fdc3d2fd93713db089f60b1f50a4743c15182f
Akash-1704 <90324028+Akash-1704@users.noreply.github.com>
2024-06-01 20:06:45 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,45 @@
+variable "aws_region" {
+  description = "Add AWS regions"
+  default     = ["ap-south-1", "us-east-1"]
+  type        = list(string)
+}
+
+
+variable "aws_vpc_name" {
+  type        = string
+  description = "Add name for your VPC."
+  default     = "demo_vpc"
+}
+
+variable "aws_vpc_cidr" {
+  type        = string
+  description = "Add CIDR block for your VPC."
+  default     = "192.168.0.0/16"
+}
+
+variable "aws_vpc_azs" {
+  type        = list(string)
+  description = "Add list of AZs available in the region that you want to use. Example ['ap-south-1a', 'ap-south-1b', 'ap-south-1c']"
+  default     = ["ap-south-1a", "ap-south-1b"]
+}
+
+
+variable "aws_vpc_public_subnets" {
+  type        = list(string)
+  description = "Add list of CIDR locks for public subnets in the vpc. Example ['192.168.11.0/24','192.168.12.0/24','192.168.13.0/24']"
+  default     = ["192.168.11.0/24", "192.168.12.0/24"]
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+
+variable "aws_sg_name" {
+  type        = string
+  description = "security group name"
+  default     = "demo_ecs_sg"
+}
+
