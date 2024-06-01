29e07930178900ef3d5a8c5552b2a09553499c73
Akash-1704 <90324028+Akash-1704@users.noreply.github.com>
2024-06-01 20:40:44 +0530

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,13 +1,11 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "ap-south-1"
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.micro"
 }
 
 variable "subnet_id" {
@@ -19,5 +17,4 @@ variable "subnet_id" {
 variable "ami_id" {
   description = "AMI ID to use for the EC2 instances"
   type        = string
-  default     = "ami-05e00961530ae1b55" 
 }
