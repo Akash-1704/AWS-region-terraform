fcbaf95a71a2f5428279f9290632f66a2298f859
Akash-1704 <90324028+Akash-1704@users.noreply.github.com>
2024-06-01 20:02:37 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,10 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.aws_vpc_name
+  cidr = var.aws_vpc_cidr
+
+  azs             = var.aws_vpc_azs
+  public_subnets  = var.aws_vpc_public_subnets
+
+ 
