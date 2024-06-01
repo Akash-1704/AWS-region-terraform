a5d83dea3fac6564703ffcd902f41f9d84ab360d
Akash-1704 <90324028+Akash-1704@users.noreply.github.com>
2024-06-01 23:57:14 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "instance" {
   ami       	= var.ami_id
   instance_type = var.instance_type
   subnet_id 	= var.subnet_id
-  region        = var.region
 
   tags = {
 	Name = "terraform-instance"
